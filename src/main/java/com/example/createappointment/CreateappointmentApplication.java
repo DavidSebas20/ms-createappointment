12f0250b4db2d7e1736554b4f5bce1a38e8e695b
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-23 12:15:24 -0500

    :tada: initialize createappointment project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/createappointment/CreateappointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.createappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateappointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateappointmentApplication.class, args);
+	}
+
+}
